9e0501e610caefb718ac97de5233c932b2543bae
Jan Beelte <jan.beelte@mailbox.org>
2023-01-17 09:50:35 +0100

    (chore) reinit google authentication, easier deployment.

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import task
+
+
+@task
+def dk_save(c):
+	c.run("docker save --output google_sync.tar google_sync-google_sync")
+
+	
+@task
+def dk_load(c):
+	c.run("docker load --input google_sync.tar")
+
+	
+@task
+def dk_copy(c):
+	c.run("scp tasks.py hetzner5:jbeelte/google_sync")
+	c.run("scp docker-compose-deploy.yml hetzner5:jbeelte/google_sync/docker-compose.yml")
+	c.run("scp google_sync.tar hetzner5:jbeelte/google_sync")
